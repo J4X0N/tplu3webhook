47e331636399cef16080a48762b0a2e93fbb4db3
Jaxon <jaxon0523@gmail.com>
2019-08-05 22:10:44 +0800

    Update tplu3.py

---
+++ tplu3.py
@@ -0,0 +1,25 @@
+import base64
+import hashlib
+import hmac
+
+channel_secret = 968e28402f9b9007fc94ab37cf688a83 # Channel secret string
+body = ... # Request body string
+hash = hmac.new(channel_secret.encode('utf-8'),
+    body.encode('utf-8'), hashlib.sha256).digest()
+signature = base64.b64encode(hash)
+# Compare X-Line-Signature request header and the signature
+
+{
+  "replyToken": "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
+  "type": "message",
+  "timestamp": 1462629479859,
+  "source": {
+    "type": "user",
+    "userId": "U4af4980629..."
+  },
+  "message": {
+    "id": "325708",
+    "type": "text",
+    "text": "Hello, world!"
+  }
+}
