229fb782be0fcdc64c3a700ed4ecce66d73ba325
Jaxon <jaxon0523@gmail.com>
2019-08-05 22:36:51 +0800

    Update tplu3.py

--- tplu3.py
+++ tplu3.py
@@ -1,3 +1,17 @@
+from linebot import LineBotApi
+from linebot.models import TextSendMessage
+from linebot.exceptions import LineBotApiError
+
+line_bot_api = LineBotApi('Ny2sBxF9f3GrKcq3ZZTqLpOyUXjFOi/HK6PHC4ecUDk7t65YlTR0hmUOAYjdZxUgx4Rgt0bzTaPQQgNMqemmrqiXyYXeI/QH0vndjSoljYfhuZiM2ivaEsYeSos27Oz+rKQYW1NwZARV7hGyjpM8PwdB04t89/1O/w1cDnyilFU=
+')
+
+try:
+    line_bot_api.push_message('<to>', TextSendMessage(text='Hello World!'))
+except LineBotApiError as e:
+    # error handle
+
+
+
 import base64
 import hashlib
 import hmac
@@ -8,18 +22,3 @@ hash = hmac.new(channel_secret.encode('utf-8'),
     body.encode('utf-8'), hashlib.sha256).digest()
 signature = base64.b64encode(hash)
 # Compare X-Line-Signature request header and the signature
-
-{
-  "replyToken": "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
-  "type": "message",
-  "timestamp": 1462629479859,
-  "source": {
-    "type": "user",
-    "userId": "U4af4980629..."
-  },
-  "message": {
-    "id": "325708",
-    "type": "text",
-    "text": "Hello, world!"
-  }
-}
